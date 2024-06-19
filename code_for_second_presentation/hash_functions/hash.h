6de5aaeb62fa8ce71c82b7c2cb0047b620cba11a
ArminKuburas <akuburass@gmail.com>
2024-06-19 10:33:37 +0300

    minor fixes

--- code_for_second_presentation/hash_functions/hash.h
+++ code_for_second_presentation/hash_functions/hash.h
@@ -6,7 +6,7 @@
 /*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 10:10:21 by akuburas          #+#    #+#             */
-/*   Updated: 2024/06/19 10:26:49 by akuburas         ###   ########.fr       */
+/*   Updated: 2024/06/19 10:32:47 by akuburas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,16 @@
 # include <stdlib.h>
 # include <stdio.h>
 
-unsigned long	create_hash(unsigned char *str);
-int				insert_hash(t_hash *hash_table, char *key, int table_size);
-t_hash			*search_hash(t_hash *hash_table, char *key, int table_size);
-int				resize_hash(int new_size, t_hash **hash_table, int table_size);
-void			free_hash(t_hash *hash_table, int table_size);
-
 typedef struct s_hash_table
 {
 	char			*key;
 	unsigned long	value;
 }	t_hash;
+
+unsigned long	create_hash(unsigned char *str);
+int				insert_hash(t_hash *hash_table, char *key, int table_size);
+t_hash			*search_hash(t_hash *hash_table, char *key, int table_size);
+int				resize_hash(int new_size, t_hash **hash_table, int table_size);
+void			free_hash(t_hash *hash_table, int table_size);
 
 #endif
